c3a2b488b1419e1578b0788ba8be62c73eb94e2a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-25 12:44:47 -0700

    imported list detail, delete, create, update views and made empty classes for each

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
 # Create your views here.
+class SnackListView(ListView):
+  pass
+
+class SnackDetailView(DetailView):
+  pass
+
+class SnackCreateView(CreateView):
+  pass
+
+class SnackUpdateView(UpdateView):
+  pass
+
+class SnackDeteletView(DeleteView):
+  pass
+
