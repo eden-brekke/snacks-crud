02f42f85b420287e640511adb9b27a3e976899cb
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-25 12:41:00 -0700

    added Snack model, with title, purchaser, and description

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,10 @@
+from webbrowser import get
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
+class Snack(models.Model):
+  title = models.CharField(max_length=256)
+  purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  description = models.TextField(default=None, blank=True)
