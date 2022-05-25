4dc8af32d374c309259b8ee3e0f2188ea776d20d
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-25 12:46:20 -0700

    added str method to display title of each object, added get_absolute_url for update functionality

--- snacks/models.py
+++ snacks/models.py
@@ -8,3 +8,10 @@ class Snack(models.Model):
   title = models.CharField(max_length=256)
   purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
   description = models.TextField(default=None, blank=True)
+  
+  def _str__(self):
+    return self.title
+  
+  def get_absolute_url(self):
+      return reverse("(snack_detail)", args=[str(self.id])
+  
